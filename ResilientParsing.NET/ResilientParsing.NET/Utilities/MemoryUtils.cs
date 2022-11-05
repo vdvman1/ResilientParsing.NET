7afcf007c0d43c11751f949ca80dff70e6670275
Stefan VDV <vdvman88@gmail.com>
2022-11-05 17:14:08 +1100

    feat: Add some memory utilities

---
+++ ResilientParsing.NET/ResilientParsing.NET/Utilities/MemoryUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResilientParsing.NET.Utilities
+{
+    /// <summary>
+    /// Memory related utilities
+    /// </summary>
+    public static class MemoryUtils
+    {
+        /// <summary>
+        /// The scale factor to use when converting between binary units, e.g. KiB -> MiB = <c>KiB * <see cref="BinaryUnitScaleFactor"/></c>
+        /// </summary>
+        public const int BinaryUnitScaleFactor = 1024;
+
+        /// <summary>
+        /// Number of bytes in a kibibyte
+        /// </summary>
+        public const int BytesPerKibibyte = BinaryUnitScaleFactor;
+
+        /// <summary>
+        /// Number of bytes in a mebibyte
+        /// </summary>
+        public const int BytesPerMebibyte = BytesPerKibibyte * BinaryUnitScaleFactor;
+
+        /// <summary>
+        /// A recommendation for the maximum number of bytes to allocate when using <c>stackalloc</c>
+        /// </summary>
+        public const int RecommendedMaxStackAllocationBytes = 1 * BytesPerKibibyte;
+    }
+
+    /// <summary>
+    /// Type specific memory related utilities
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class MemoryUtils<T>
+    {
+        public static readonly int RecommendMaxStackAllocationLength = MemoryUtils.RecommendedMaxStackAllocationBytes / Unsafe.SizeOf<T>();
+    }
+}
---
+++ ResilientParsing.NET/Tests.ResilientParsing.NET/MemoryUtilsTests.cs
@@ -0,0 +1,29 @@
+﻿using ResilientParsing.NET.Builders;
+using ResilientParsing.NET.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.ResilientParsing.NET
+{
+    public class MemoryUtilsTests
+    {
+        [Fact]
+        public void KibibytesIsCorrectSize()
+        {
+            Assert.Equal(1024, MemoryUtils.BytesPerKibibyte);
+        }
+
+        [Fact]
+        public void MebibytesIsCorrectSize()
+        {
+            Assert.Equal(1048576, MemoryUtils.BytesPerKibibyte);
+        }
+
+        // TODO: How to test MemoryUtils<T>.RecommendMaxStackAllocationLength?
+        // The value of MemoryUtils.RecommendedMaxStackAllocationBytes may change in future, so can't hardcode the lengths
+        // Calculating the lengths would be done the same as the implementation being tested, so would always pass
+    }
+}
