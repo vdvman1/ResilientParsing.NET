10f6e7b553b1d79dd5d0a10cf4e2370716c0aafb
Stefan VDV <vdvman88@gmail.com>
2022-11-06 13:04:28 +1100

    docs: UnsafeSpanExtensions

--- ResilientParsing.NET/ResilientParsing.NET/Utilities/UnsafeSpanExtensions.cs
+++ ResilientParsing.NET/ResilientParsing.NET/Utilities/UnsafeSpanExtensions.cs
@@ -11,6 +11,17 @@ namespace ResilientParsing.NET.Utilities
 {
     internal static class UnsafeSpanExtensions
     {
+        /// <summary>
+        /// Get the value at <paramref name="index"/> without doing any bounds checks
+        /// </summary>
+        /// <typeparam name="T">The type of items in the <see cref="ReadOnlySpan{T}"/></typeparam>
+        /// <param name="span">The <see cref="ReadOnlySpan{T}"/> to fetch the value from</param>
+        /// <param name="index">The zero-based index of the element to get.</param>
+        /// <returns></returns>
+        /// <remarks>
+        /// It is the caller's responsibility to ensure the index is within the bounds of the span.
+        /// Indexing outside the bounds of the span is undefined behaviour
+        /// </remarks>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T GetUnchecked<T>(this ReadOnlySpan<T> span, int index)
         {
@@ -21,6 +32,17 @@ namespace ResilientParsing.NET.Utilities
             return ptr;
         }
 
+        /// <summary>
+        /// Get a reference to the value at <paramref name="index"/> without doing any bounds checks
+        /// </summary>
+        /// <typeparam name="T">The type of items in the <see cref="ReadOnlySpan{T}"/></typeparam>
+        /// <param name="span">The <see cref="ReadOnlySpan{T}"/> to fetch the value from</param>
+        /// <param name="index">The zero-based index of the element to get.</param>
+        /// <returns></returns>
+        /// <remarks>
+        /// It is the caller's responsibility to ensure the index is within the bounds of the span.
+        /// Indexing outside the bounds of the span is undefined behaviour
+        /// </remarks>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ref T AtUnchecked<T>(this Span<T> span, int index)
         {
@@ -31,6 +53,16 @@ namespace ResilientParsing.NET.Utilities
             return ref ptr;
         }
 
+        /// <summary>
+        /// Creates a new span over a target array, without doing a null check
+        /// </summary>
+        /// <typeparam name="T">The type of the array.</typeparam>
+        /// <param name="array">The array to convert.</param>
+        /// <returns>The span representation of the array.</returns>
+        /// <remarks>
+        /// It is the callers responsibility to ensure the array is not null.
+        /// Creating a span from a null array with this method is undefined behaviour
+        /// </remarks>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Span<T> NonNullAsSpanUnchecked<T>(this T[] array)
         {
@@ -39,16 +71,34 @@ namespace ResilientParsing.NET.Utilities
             return MemoryMarshal.CreateSpan(ref MemoryMarshal.GetArrayDataReference(array), array.Length);
         }
 
+        /// <summary>
+        /// Forms a slice out of the current span that begins at a specified index, without doing any bounds checks
+        /// </summary>
+        /// <typeparam name="T">The type of items in the <see cref="Span{T}"/></typeparam>
+        /// <param name="span">The <see cref="Span{T}"/> to slice</param>
+        /// <param name="start">The index at which to begin the slice.</param>
+        /// <returns>A span that consists of all elements of the current span from start to the end of the span.</returns>
+        /// <remarks>
+        /// It is the callers responsibility to ensure that <paramref name="span"/> is between 0 and the length of the span.
+        /// Slicing with a start index outside the valid range is undefined behaviour.
+        /// </remarks>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Span<T> SliceUnchecked<T>(this Span<T> span, int offset)
+        public static Span<T> SliceUnchecked<T>(this Span<T> span, int start)
         {
-            Debug.Assert(offset <= span.Length);
+            Debug.Assert(start <= span.Length);
 
             ref T ptr = ref MemoryMarshal.GetReference(span);
-            ptr = ref Unsafe.Add(ref ptr, offset);
-            return MemoryMarshal.CreateSpan(ref ptr, span.Length - offset);
+            ptr = ref Unsafe.Add(ref ptr, start);
+            return MemoryMarshal.CreateSpan(ref ptr, span.Length - start);
         }
 
+        /// <summary>
+        /// Forms a slice out of the current span that begins at the start of the span for a specified length.
+        /// </summary>
+        /// <typeparam name="T">The type of items in the <see cref="Span{T}"/></typeparam>
+        /// <param name="span">The <see cref="Span{T}"/> to slice</param>
+        /// <param name="length">The desired length for the slice.</param>
+        /// <returns>A span that consists of <paramref name="length"/> elements from the current span starting at the start of the span</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Span<T> SliceLengthUnchecked<T>(this Span<T> span, int length)
         {
